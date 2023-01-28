4b6876b779a92e9bd29e73491f25cbc8206b189d
Aleazar yilma <yilma4060@gmail.com>
2023-01-28 20:44:24 +0300

    fix eslint linter issue

--- js/modules/popup.js
+++ js/modules/popup.js
@@ -1,9 +1,9 @@
-const popupcontainer = document.createElement("section");
+const popupcontainer = document.createElement('section');
 
 const popup = (name, img, desc, tech, liveVersion, projectSource, index) => {
-  popupcontainer.classList.add("mobile-popup-container");
-  popupcontainer.setAttribute("data-visible", "false");
-  popupcontainer.classList.add("display-none");
+  popupcontainer.classList.add('mobile-popup-container');
+  popupcontainer.setAttribute('data-visible', 'false');
+  popupcontainer.classList.add('display-none');
 
   popupcontainer.innerHTML = `
   <div class="icon-image">
@@ -47,25 +47,25 @@ const popup = (name, img, desc, tech, liveVersion, projectSource, index) => {
     techContainer.innerHTML += `<li class="popup-tech-li">${tec}</li>`;
   });
 
-  const divbackcross = document.querySelector(".cross-popup-icon");
-  document.addEventListener("click", (event) => {
-    const popup = document.querySelector(".mobile-popup-container");
-    const visibility = popup.getAttribute("data-visible");
-    if (event.target.closest(".mobile-popup-container")) return;
-    if (visibility === "true") {
-      popup.setAttribute("data-visible", "onRight");
+  const divbackcross = document.querySelector('.cross-popup-icon');
+  document.addEventListener('click', (event) => {
+    const popup = document.querySelector('.mobile-popup-container');
+    const visibility = popup.getAttribute('data-visible');
+    if (event.target.closest('.mobile-popup-container')) return;
+    if (visibility === 'true') {
+      popup.setAttribute('data-visible', 'onRight');
     }
-    document.body.style.overflow = "auto";
+    document.body.style.overflow = 'auto';
   });
 
-  divbackcross.addEventListener("click", () => {
-    const popup = document.querySelector(".mobile-popup-container");
-    const visibility = popup.getAttribute("data-visible");
-    const techDiv = popup.querySelector(".title-and-tech");
-    const popupTech = popup.querySelector(".popup-tech");
+  divbackcross.addEventListener('click', () => {
+    const popup = document.querySelector('.mobile-popup-container');
+    const visibility = popup.getAttribute('data-visible');
+    const techDiv = popup.querySelector('.title-and-tech');
+    const popupTech = popup.querySelector('.popup-tech');
 
-    if (visibility === "true") {
-      popup.setAttribute("data-visible", "onRight");
+    if (visibility === 'true') {
+      popup.setAttribute('data-visible', 'onRight');
     }
 
     setTimeout(() => {
@@ -84,9 +84,9 @@ const popup = (name, img, desc, tech, liveVersion, projectSource, index) => {
         popup.removeChild(child);
       });
       popup.parentElement.removeChild(popup);
-      popup.setAttribute("data-visible", "false");
+      popup.setAttribute('data-visible', 'false');
     }, 200);
-    document.body.style.overflow = "auto";
+    document.body.style.overflow = 'auto';
   });
 };
 
